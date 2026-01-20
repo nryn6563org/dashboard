eaf45e31ceaab2bf9c64b80d2b89b9a71119aeff
jhLee <77824683+nryn6563org@users.noreply.github.com>
2026-01-20 15:17:15 +0900

    feat: Configure Nuxt for static site generation with a dashboard base path and add a `gh-pages` deployment script.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,53 +1,38 @@
 export default {
+  target: "static",
+  router: {
+    base: "/dashboard/"
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'dashboard-system',
+    title: "dashboard-system",
     htmlAttrs: {
-      lang: 'ko'
+      lang: "ko"
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    meta: [{ charset: "utf-8" }, { name: "viewport", content: "width=device-width, initial-scale=1" }, { hid: "description", name: "description", content: "" }, { name: "format-detection", content: "telephone=no" }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/css/tailwind.css',
-  ],
+  css: ["@/assets/css/tailwind.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/vue-grid-layout.js',
-    '@/plugins/lucide.js'
-  ],
+  plugins: ["@/plugins/vue-grid-layout.js", "@/plugins/lucide.js"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxtjs/tailwindcss'
-  ],
+  buildModules: ["@nuxtjs/tailwindcss"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxtjs/axios',
-  ],
+  modules: ["@nuxtjs/axios"],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: ['lucide-vue', 'vue-grid-layout'],
+    transpile: ["lucide-vue", "vue-grid-layout"],
     babel: {
-      plugins: [
-        '@babel/plugin-proposal-optional-chaining',
-        '@babel/plugin-proposal-nullish-coalescing-operator'
-      ]
+      plugins: ["@babel/plugin-proposal-optional-chaining", "@babel/plugin-proposal-nullish-coalescing-operator"]
     }
   }
-}
+};
